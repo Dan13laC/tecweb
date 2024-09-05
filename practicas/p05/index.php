d1119ab3041c9ec15b87ca7663a179d0442770d2
daan <danicamacho22.DC@gmail.com>
2024-09-04 20:04:39 -0600

    Ingreso de ejercicio 1 a index.php

--- practicas/p05/index.php
+++ practicas/p05/index.php
@@ -7,5 +7,23 @@
     </head>
 
     <body>
-        
+        <h3>Ejercicio 1</h3>
+            <?php
+                $_myvar = '$_myvar es una variable porque empieza con $ y contiene carácteres válidos';
+                $_7myvar = '$_7myvar es una variable porque empieza con $ y contiene carácteres válidos';
+                //myvar No es una variable porque no empieza con $
+                $myvar = '$myvar es una variable porque empieza con $ y contiene carácteres válidos';
+                $var7 = '$var7 es una variable porque empieza con $ y contiene carácteres válidos';
+                $_element1 = '$_element1 es una variable porque empieza con $ y contiene carácteres válidos';
+                //$house*5 No es una variable porque el * es un caracter no admitido
+                echo $_myvar.'<br>';
+                echo $_7myvar.'<br>';
+                echo 'myvar no es una variable válida porque no comienza con $<br>';
+                echo $myvar.'<br>';
+                echo $var7.'<br>';
+                echo $_element1.'<br>';
+                echo '$house*5 no es una variable válida porque el * es un caracter no admitido<br>';
+            ?>
+
+    </body>    
 </html>
