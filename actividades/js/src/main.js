bd90a63ada8d5b2a744ed2a35331f787edf3d2ad
daan <danicamacho22.DC@gmail.com>
2024-09-27 19:01:22 -0600

    Ingreso de ejercicios correspondientes a JS04 y JS05 a main

--- actividades/js/src/main.js
+++ actividades/js/src/main.js
@@ -156,3 +156,147 @@ function casecolor(){
         break;
     }
 }
+
+function ciclowhile(){
+    var x;
+    x=1;
+    var div1 = document.getElementById('whilejs04');
+    while (x<=100) {
+        div1.innerHTML = div1.innerHTML+'<p>'+x+'</p>';
+        x=x+1;
+    }
+}
+
+function whilesuma(){
+    var x=1;
+    var suma=0;
+    var valor;
+    var div1 = document.getElementById('while2js04');
+    while (x<=5){
+        valor = prompt('Ingresa el valor:', '');
+        valor = parseInt(valor);
+        suma = suma+valor;
+        x = x+1;
+    }
+    div1.innerHTML = '<p>La suma de los valores es '+suma+'</p>';
+}
+
+function dowhile(){
+    var valor;
+    do{
+        valor = prompt('Ingresa un valor entre 0 y 999:', '');
+        valor = parseInt(valor);
+
+        var div1 = document.getElementById('dowhilejs04');
+        div1.innerHTML = div1.innerHTML+'<p>El valor '+valor+' tiene</p>';
+        if (valor<10)
+            div1.innerHTML= div1.innerHTML+'<p>Tiene 1 dígitos </p>';
+        else{
+            if (valor<100) {
+                div1.innerHTML=div1.innerHTML+'<p>Tiene 2 dígitos </p>';
+            }
+            else {
+                div1.innerHTML=div1.innerHTML+'<p>Tiene 3 dígitos </p>';
+            }
+        }
+    }while(valor!=0);
+}
+
+function iterativofor(){
+    var f;
+    var div1 = document.getElementById('forjs04');
+    for(f=1; f<=10; f++){
+        div1.innerHTML = div1.innerHTML + f + " ";
+    }
+}
+
+function repeticion(){
+    var div1 = document.getElementById('mensaje1js05');
+    var div2 = document.getElementById('mensaje2js05');
+    var div3 = document.getElementById('mensaje3js05');
+    div1.innerHTML='<p>Cuidado<br>Ingresa tu documento correctamente</p>';
+    div2.innerHTML='<p>Cuidado<br>Ingresa tu documento correctamente</p>';
+    div3.innerHTML='<p>Cuidado<br>Ingresa tu documento correctamente</p>';
+}
+
+function funcion(){
+    var div1 = document.getElementById('funcionjs05');
+
+    function mostrarMensaje() {
+        div1.innerHTML=div1.innerHTML+'<p>Cuidado<br>Ingresa tu documento correctamente</p>';
+/*        document.write('Cuidado<br>');
+        document.write('Ingresa tu documento correctamente<br>');
+*/    }
+    mostrarMensaje();
+    mostrarMensaje();
+    mostrarMensaje();
+}
+
+function rango(){
+    var div1 = document.getElementById('rangojs05');
+    div1.innerHTML="<p>";
+    function mostrarRango(x1,x2) {
+        var inicio;
+        for(inicio=x1; inicio<=x2; inicio++) {
+            div1.innerHTML= div1.innerHTML + inicio + ' ';
+//            document.write(inicio+' ');
+        
+        }
+    }
+    div1.innerHTML= div1.innerHTML +"</p>";
+    
+    var valor1,valor2;
+    valor1 = prompt('Ingresa el valor inferior:', '');
+    valor1 = parseInt(valor1);
+    valor2 = prompt('Ingresa el valor superior:', '');
+    valor2 = parseInt(valor2);
+    mostrarRango(valor1,valor2);
+}
+
+function convc1(){
+    function convertirCastellano(x) {
+        if(x==1)
+            return "uno";
+        else
+            if(x==2)  
+                return "dos";
+            else
+                if(x==3)
+                    return "tres";
+                else
+                    if(x==4)
+                        return "cuatro";        
+                    else
+                        if(x==5)
+                            return "cinco";
+                        else
+                            return "valor incorrecto";
+    }
+    var div1 = document.getElementById('convC1js05');
+    
+    var valor = prompt("Ingresa un valor entre 1 y 5", "");
+    valor = parseInt(valor);
+    var r = convertirCastellano(valor);
+    div1.innerHTML= '<p>'+r+'</p>';
+    
+}
+
+function convc2(){
+    function convertirCastellano(x) {
+        switch (x) {
+        case 1: return "uno";
+        case 2: return "dos";
+        case 3: return "tres";
+        case 4: return "cuatro";
+        case 5: return "cinco";
+        default: return "valor incorrecto";
+        }    
+    }
+    var div1 = document.getElementById('convC2js05');
+    
+    var valor = prompt("Ingresa un valor entre 1 y 5", "");
+    valor = parseInt(valor);
+    var r = convertirCastellano(valor);
+    div1.innerHTML= '<p>'+r+'</p>';
+
+}
